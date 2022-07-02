df2e73849ff12bfbdebb7bb6035eebde4b24e9af
PaulKitonyi <paulmucimah@gmail.com>
2022-07-02 23:50:55 +0300

    add excel download endpoint

--- controllers/saving.js
+++ controllers/saving.js
@@ -1,5 +1,6 @@
-const { Saving, validate } = require('../models/saving');
+const excelJS = require('exceljs');
 const _ = require('lodash');
+const { Saving, validate } = require('../models/saving');
 
 exports.createSaving = async (req, res) => {
   const { error } = validate(req.body);
@@ -16,4 +17,51 @@ exports.createSaving = async (req, res) => {
 
 exports.getSavings = async (req, res) => {
   res.send(res.paginatedResults);
+};
+
+exports.exportSavings = async (req, res) => {
+  const dateNow = Date.now();
+
+  let savings = await Saving.find();
+
+  const workbook = new excelJS.Workbook();
+  const worksheet = workbook.addWorksheet('Savings Report');
+  const path = './files';
+  const fileName = dateNow + '-' + 'savingsreport.xlsx';
+
+  worksheet.columns = [
+    { header: 'Account Number', key: 'account', width: 10 },
+    { header: 'Saved Amount', key: 'amount', width: 10 },
+    { header: 'Description', key: 'description', width: 10 },
+    { header: 'Date', key: 'createdAt', width: 10 },
+  ];
+
+  let counter = 1;
+  savings.forEach((saving) => {
+    worksheet.addRow(saving);
+    counter++;
+  });
+
+  worksheet.getRow(1).eachCell((cell) => {
+    cell.font = { bold: true };
+  });
+
+  try {
+    const data = await workbook.xlsx
+      .writeFile(`${path}/${fileName}`)
+      .then(() => {
+        res.send({
+          status: 'success',
+          message: 'file successfully downloaded',
+          path: `${path}/${fileName}`,
+        });
+      });
+  } catch (err) {
+    res.send({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // res.send(savings);
 };
--- routes/savings.js
+++ routes/savings.js
@@ -2,10 +2,14 @@ const { Saving } = require('../models/saving');
 const express = require('express');
 const router = express.Router();
 const { paginatedResults } = require('../lib/pagination');
-const { createSaving, getSavings } = require('../controllers/saving');
+const {
+  createSaving,
+  getSavings,
+  exportSavings,
+} = require('../controllers/saving');
 
 router.post('/save', createSaving);
-
 router.get('/savings', paginatedResults(Saving), getSavings);
+router.get('/savings/downloadExcel', exportSavings);
 
 module.exports = router;
